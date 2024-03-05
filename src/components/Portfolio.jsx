291326499b558d825c9a36c47015dab0f15f1406
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2024-03-05 15:37:25 -0800

    fixing images again

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,11 @@
 import Project from "./Project";
-import audiophiles from "../assets/images/portfolio/audiophiles.png"
+import audiophiles from "../assets/images/portfolio/audiophiles.png";
+import friends from "../assets/images/portfolio/friends.png";
+import employeeTracker from "../assets/images/portfolio/employee_tracker.png";
+import textEditor from "../assets/images/portfolio/text_editor.png";
+import weather from "../assets/images/portfolio/weather_dashboard.png";
+import palette from "../assets/images/portfolio/palette_pair.png";
+
 
 function Portfolio() {
   const projects = [
@@ -15,35 +21,35 @@ function Portfolio() {
       title: "Friends and Thoughts",
       github: "https://github.com/Erik-Aku/Friends-And-Thoughts",
       deployed: "https://drive.google.com/file/d/1-NFJvqL7Pv6ya0TN8curi-TXwnFHtcdK/view?usp=sharing",
-      image: "friends.png",
+      image: friends,
     },
     {
       id: 2,
       title: "Employee Tracker",
       github: "https://github.com/Erik-Aku/Employee-Tracker",
       deployed: "https://drive.google.com/file/d/1CDOX7aYKJDaIPi0SbFw7ndk3elmUPyLB/view?usp=sharing",
-      image: "employee_tracker.png",
+      image: employeeTracker,
     },
     {
       id: 3,
       title: "Text-Editor-PWA",
       github: "https://github.com/Erik-Aku/Text-Editor-PWA",
       deployed: "https://text-editor-pwa-hgt1.onrender.com/",
-      image: "text_editor.png",
+      image: textEditor,
     },
     {
       id: 4,
       title: "Weather-Dashboard",
       github: "https://github.com/Erik-Aku/Weather-Dashboard",
       deployed: "https://erik-aku.github.io/Weather-Dashboard/",
-      image: "weather_dashboard.png",
+      image: weather,
     },
     {
       id: 5,
       title: "Palette-Pair",
       github: "https://github.com/Erik-Aku/Palette-Pair",
       deployed: "https://erik-aku.github.io/Palette-Pair/",
-      image: "palette_pair.png",
+      image: palette,
     },
   ];
   return (
